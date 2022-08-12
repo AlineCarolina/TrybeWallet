ef7240512d0f9542f63d896dfafd14bd1f0f16ed
Aline <eualine2017@gmail.com>
2022-08-12 18:48:19 -0300

    feat: adding remove action

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,8 @@ export const SET_EMAIL = 'SET_EMAIL';
 export const GET_VALUE = 'GET_VALUE';
 export const GET_ERROR = 'GET_ERROR';
 export const GET_EXPENSE = 'GET_EXPENSE';
-export const REMOVE_EXPENSES = 'REMOVE_EXPENSES';
+export const REMOVE_EXPENSE = 'REMOVE_EXPENSE';
+export const UPDATE_EXPENSES_TOTAL = 'UPDATE_EXPENSES_TOTAL';
 
 export const setEmailValue = (payload) => (
   {
@@ -26,9 +27,12 @@ export const addExpense = (payload) => (
     type: GET_EXPENSE, payload,
   });
 
-export const removeExpenses = (payload) => ({
-  type: REMOVE_EXPENSES, payload,
-});
+const updateExpensesTotal = () => ({ type: UPDATE_EXPENSES_TOTAL });
+
+export const removeExpense = (id) => (dispatch) => {
+  dispatch({ type: REMOVE_EXPENSE, id });
+  dispatch(updateExpensesTotal());
+};
 
 // thunk
 export const getValueThunk = () => (
