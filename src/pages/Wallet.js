2df96507c591d05e7fb49479bf72a5aad2123857
Aline <eualine2017@gmail.com>
2022-08-12 18:50:30 -0300

    fix: refactoring and adding css to wallet page

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -2,15 +2,16 @@ import React from 'react';
 import Header from '../components/Header';
 import Form from '../components/Form';
 import WalletTable from '../components/WalletTable';
+import '../styles/Wallet.css';
 
 class Wallet extends React.Component {
   render() {
     return (
-      <>
+      <div id="pricipal-div-wallet">
         <Header />
         <Form />
         <WalletTable />
-      </>
+      </div>
     );
   }
 }
